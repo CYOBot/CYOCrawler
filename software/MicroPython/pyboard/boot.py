439f90580dcb836902a6625769e95d98b0d4ce73
Nguyen Phuong Duy <duyn@princeton.edu>
2023-10-07 12:13:48 -0400

    add schedule in boot.py to check for new main.py in sd card to update firmware

--- software/MicroPython/pyboard/boot.py
+++ software/MicroPython/pyboard/boot.py
@@ -11,3 +11,23 @@ sys.path.append("/sd")
 
 # import os
 # os.chdir("/sd")
+
+# check to see if there is main.py in SD card, if there is, overwrite the current file and restart
+import os
+
+try:
+    os.stat("/sd/main.py")
+    print("Found main.py, overwrite the main.py")
+    
+    # remove current main.py
+    os.remove("main.py")
+    
+    with open('main.py', 'wt') as outfile:
+        file = open('/sd/main.py', 'rt').read()
+        outfile.write(file)
+    
+    # remove main.py in SD card
+    os.remove("/sd/main.py")
+
+except:
+    print("No new file, continue")
